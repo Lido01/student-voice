4dea061e817c467ebb37d874d1ed3ea7e1df3ce4
Lidetu <lidosinafanis@gmail.com>
2026-04-30 02:58:22 +0300

    models for backend feedback

---
+++ backend/apps/feedback/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Feedback(models.Model):
+    CATEGORY_CHOICES = (
+        ('academic', 'Academic'),
+        ('facility', 'Facility'),
+        ('service', 'Service'),
+    )
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('review', 'In Review'),
+        ('resolved', 'Resolved'),
+    )
+
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student_feedbacks', null=True, blank=True)
+    anonymous = models.BooleanField(default=False)
+    subject = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='feedback/', blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    target = models.CharField(max_length=50)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
