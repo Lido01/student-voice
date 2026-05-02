f88b32391a80afba14cd14b688bba1c84b7726a6
Lidetu <lidosinafanis@gmail.com>
2026-05-02 15:45:09 +0300

    setting up main mobile congif library and packages

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import MainNavigator from './src/navigation/MainNavigator';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Auth Stack */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+        {/* Main App Stack (The shell with Navbar) */}
+        <Stack.Screen name="MainNavigator" component={MainNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
